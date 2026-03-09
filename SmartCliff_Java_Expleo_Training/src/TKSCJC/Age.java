004f9c5a4bc9b2796859b71e8377b8c5f06c3fbf
TAMILKUMAR0027 <tamilkumar0027@gmail.com.com>
2026-03-09 15:21:12 +0530

    WS

---
+++ SmartCliff_Java_Expleo_Training/src/TKSCJC/Age.java
@@ -0,0 +1,18 @@
+package TKSCJC;
+
+import java.util.Scanner;
+
+public class Age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the name");
+		String name=s.next();
+		System.out.println("Enter the age");
+		int age =s.nextInt();
+		System.out.println(name);
+		System.out.println(age);
+	}
+
+}
