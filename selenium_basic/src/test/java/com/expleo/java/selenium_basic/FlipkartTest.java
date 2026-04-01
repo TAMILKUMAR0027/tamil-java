7476dcdcc58ed85c1b5772fb7d83de3a0eb72b3f
TAMILKUMAR0027 <tamilkumar0027@gmail.com.com>
2026-04-01 11:34:42 +0530

    kk

--- selenium_basic/src/test/java/com/expleo/java/selenium_basic/FlipkartTest.java
+++ selenium_basic/src/test/java/com/expleo/java/selenium_basic/FlipkartTest.java
@@ -1,26 +1,26 @@
 package com.expleo.java.selenium_basic;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
-
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.*;
 public class FlipkartTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.flipkart.com/");
+	
+		WebDriver d=new EdgeDriver();
+		d.manage().window().maximize();
+		d.get("https://www.flipkart.com/");
 		String st="https://www.flipkart.com/";
-		System.out.println("Page title : "+driver.getTitle()+" Length of the title :"+driver.getTitle().length());
-		if(driver.getCurrentUrl().equals(st)) {
+		System.out.println("Page title : "+d.getTitle()+" Length of the title :"+d.getTitle().length());
+		if(d.getCurrentUrl().equals(st)) {
 			System.out.println("Url is crt");
 		}else {
 			System.out.println("Fail");
 		}
-		System.out.println(driver.getPageSource());
-		System.out.println(driver.getPageSource().length());
+		System.out.println(d.getPageSource());
+		System.out.println(d.getPageSource().length());
+		d.quit();
 		
 	}
 
